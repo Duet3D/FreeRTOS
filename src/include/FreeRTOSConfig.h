80a3a5c0465aaa4addca73495437d280d9f0503f
David Crocker <dcrocker@eschertech.com>
2025-07-15 14:27:57 +0100

    Changed configUSE_TIME_SLICING from 0 to 1

--- src/include/FreeRTOSConfig.h
+++ src/include/FreeRTOSConfig.h
@@ -77,7 +77,7 @@ extern uint32_t SystemCoreClock;
  * configUSE_TIME_SLICING to 0 to prevent the scheduler switching between Ready
  * state tasks just because there was a tick interrupt.  See
  * https://freertos.org/single-core-amp-smp-rtos-scheduling.html. */
-#define configUSE_TIME_SLICING                     0
+#define configUSE_TIME_SLICING                     1
 
 /* Set configUSE_PORT_OPTIMISED_TASK_SELECTION to 1 to select the next task to
  * run using an algorithm optimised to the instruction set of the target hardware -
